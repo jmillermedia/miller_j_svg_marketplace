4d1a53386633778dc25fca4a97385fb86850cb6c
Jon <59712501+jmillermedia@users.noreply.github.com>
2020-05-23 12:37:02 -0400

    Inital Buttons Added
    Added buttons to each image and made javascript show each button selected. Moving on to make the image selectable.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 // JAVASCRIPT START
+
+(() => {
+	console.log('JS is working.')
+
+	let theIcons = document.querySelectorAll('.icon');
+
+	function logID() {
+		console.log("You have clicked on: " + this.id);
+	}
+
+	theIcons.forEach(icon => icon.addEventListener('click', logID));
+})();
