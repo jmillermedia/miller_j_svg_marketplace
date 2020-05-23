5aa5668bbb7b382a1df965521c6a2004aea5b306
Jon <59712501+jmillermedia@users.noreply.github.com>
2020-05-23 14:13:15 -0400

    Sorting Issues
    Having an issue with the dataset coming back with a value of "undefined". Not sure why it's not taking.

--- js/script.js
+++ js/script.js
@@ -4,10 +4,21 @@
 	console.log('JS is working.')
 
 	let theIcons = document.querySelectorAll('.icon');
+	mainImage = document.querySelector(".iconSelection");
 
 	function logID() {
 		console.log("You have clicked on: " + this.id);
 	}
 
+	function switchImage() {
+		// console.log('clicked a button')
+
+		let currentImage = this.dataset.character + ".svg";
+		
+		mainImage.data = "images/" + currentImage;
+	}
+
 	theIcons.forEach(icon => icon.addEventListener('click', logID));
+	theIcons.forEach(button => button.addEventListener('click', switchImage));
+
 })();
