43a36453a69211e14559bcd11aeec1a4f228c113
Jon <59712501+jmillermedia@users.noreply.github.com>
2020-07-10 22:42:37 -0400

    UI_UXAssignment complete
    Finished the assignment.

--- js/script.js
+++ js/script.js
@@ -10,21 +10,21 @@
 
 	theIcons.forEach(icon => icon.addEventListener('click', logID));
 	}
-	
+
 	let emailDrop = document.querySelectorAll('.emailButton'),
-	emailButton = document.querySelectorAll('.emailDropdown'),
-	imageSelect = document.querySelectorAll('.icoSelect'),
-	imageButton = document.querySelectorAll('.collectionButton');
+	// emailButton = document.querySelectorAll('.emailDropdown'),
+		imageSelect = document.querySelectorAll('.icoSelect'),
+		imageButton = document.querySelectorAll('.collectionButton');
 
 
 	function dropUp() {
-		imageSelect[0].classList.toggle('show');
+		this.previousElementSibling.classList.toggle('show');
 	}
 
 
 	function dropDown() {
-		emailButton[0].classList.toggle('show');
-		emailDrop[0].getElementsByTagName('img').classList.toggle('.animateEmail')
+		this.nextElementSibling.classList.toggle('show');
+		this.querySelector('img').classList.toggle('animateEmail')
 }
 
 	emailDrop.forEach(drop => drop.addEventListener('click', dropDown));
